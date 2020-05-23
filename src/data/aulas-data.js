514c584acf6d03798789018232590f7f5d1fc773
Rafael Muttoni <rmuttonic@gmail.com>
2020-05-23 18:05:27 -0300

    Aulas data for future section

---
+++ src/data/aulas-data.js
@@ -0,0 +1,200 @@
+export default [
+  {
+    platforma: 'Excel',
+    niveis: [
+      {
+        name: 'Nível Básico',
+        description: 'Aprenda o básico sobre planilhas.',
+        icon: 'chart-line',
+        price: '369,00',
+        link: 'https://paypal.com'
+      },
+      {
+        name: 'Nível Intermediário',
+        description: 'Aprenda o básico sobre planilhas.',
+        icon: 'chart-line',
+        price: '369,00',
+        link: 'https://paypal.com'
+      },
+      {
+        name: 'Nível Básico',
+        description: 'Aprenda o básico sobre planilhas.',
+        icon: 'chart-line',
+        price: '369,00',
+        link: 'https://paypal.com'
+      },
+      {
+        name: 'Nível Básico',
+        description: 'Aprenda o básico sobre planilhas.',
+        icon: 'chart-line',
+        price: '369,00',
+        link: 'https://paypal.com'
+      }
+    ]
+  },
+  {
+    platforma: 'Powerpoint',
+    niveis: [
+      {
+        name: 'Nível Básico',
+        description: 'Aprenda o básico sobre planilhas.',
+        icon: 'chart-line',
+        price: '369,00',
+        link: 'https://paypal.com'
+      },
+      {
+        name: 'Nível Intermediário',
+        description: 'Aprenda o básico sobre planilhas.',
+        icon: 'chart-line',
+        price: '369,00',
+        link: 'https://paypal.com'
+      },
+      {
+        name: 'Nível Básico',
+        description: 'Aprenda o básico sobre planilhas.',
+        icon: 'chart-line',
+        price: '369,00',
+        link: 'https://paypal.com'
+      },
+      {
+        name: 'Nível Básico',
+        description: 'Aprenda o básico sobre planilhas.',
+        icon: 'chart-line',
+        price: '369,00',
+        link: 'https://paypal.com'
+      }
+    ]
+  },
+  {
+    platforma: 'Word',
+    niveis: [
+      {
+        name: 'Nível Básico',
+        description: 'Aprenda o básico sobre planilhas.',
+        icon: 'chart-line',
+        price: '369,00',
+        link: 'https://paypal.com'
+      },
+      {
+        name: 'Nível Intermediário',
+        description: 'Aprenda o básico sobre planilhas.',
+        icon: 'chart-line',
+        price: '369,00',
+        link: 'https://paypal.com'
+      },
+      {
+        name: 'Nível Básico',
+        description: 'Aprenda o básico sobre planilhas.',
+        icon: 'chart-line',
+        price: '369,00',
+        link: 'https://paypal.com'
+      },
+      {
+        name: 'Nível Básico',
+        description: 'Aprenda o básico sobre planilhas.',
+        icon: 'chart-line',
+        price: '369,00',
+        link: 'https://paypal.com'
+      }
+    ]
+  },
+  {
+    platforma: 'PowerBI',
+    niveis: [
+      {
+        name: 'Nível Básico',
+        description: 'Aprenda o básico sobre planilhas.',
+        icon: 'chart-line',
+        price: '369,00',
+        link: 'https://paypal.com'
+      },
+      {
+        name: 'Nível Intermediário',
+        description: 'Aprenda o básico sobre planilhas.',
+        icon: 'chart-line',
+        price: '369,00',
+        link: 'https://paypal.com'
+      },
+      {
+        name: 'Nível Básico',
+        description: 'Aprenda o básico sobre planilhas.',
+        icon: 'chart-line',
+        price: '369,00',
+        link: 'https://paypal.com'
+      },
+      {
+        name: 'Nível Básico',
+        description: 'Aprenda o básico sobre planilhas.',
+        icon: 'chart-line',
+        price: '369,00',
+        link: 'https://paypal.com'
+      }
+    ]
+  },
+  {
+    platforma: 'PowerApps',
+    niveis: [
+      {
+        name: 'Nível Básico',
+        description: 'Aprenda o básico sobre planilhas.',
+        icon: 'chart-line',
+        price: '369,00',
+        link: 'https://paypal.com'
+      },
+      {
+        name: 'Nível Intermediário',
+        description: 'Aprenda o básico sobre planilhas.',
+        icon: 'chart-line',
+        price: '369,00',
+        link: 'https://paypal.com'
+      },
+      {
+        name: 'Nível Básico',
+        description: 'Aprenda o básico sobre planilhas.',
+        icon: 'chart-line',
+        price: '369,00',
+        link: 'https://paypal.com'
+      },
+      {
+        name: 'Nível Básico',
+        description: 'Aprenda o básico sobre planilhas.',
+        icon: 'chart-line',
+        price: '369,00',
+        link: 'https://paypal.com'
+      }
+    ]
+  },
+  {
+    platforma: 'Flow',
+    niveis: [
+      {
+        name: 'Nível Básico',
+        description: 'Aprenda o básico sobre planilhas.',
+        icon: 'chart-line',
+        price: '369,00',
+        link: 'https://paypal.com'
+      },
+      {
+        name: 'Nível Intermediário',
+        description: 'Aprenda o básico sobre planilhas.',
+        icon: 'chart-line',
+        price: '369,00',
+        link: 'https://paypal.com'
+      },
+      {
+        name: 'Nível Básico',
+        description: 'Aprenda o básico sobre planilhas.',
+        icon: 'chart-line',
+        price: '369,00',
+        link: 'https://paypal.com'
+      },
+      {
+        name: 'Nível Básico',
+        description: 'Aprenda o básico sobre planilhas.',
+        icon: 'chart-line',
+        price: '369,00',
+        link: 'https://paypal.com'
+      }
+    ]
+  }
+];
