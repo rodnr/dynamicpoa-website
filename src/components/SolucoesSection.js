da23ffbf38d519a57a5750feb94844d909d3fc6b
Rafael Muttoni <hi@rafaelmuttoni.com>
2020-08-29 18:05:58 -0300

    Texto seção 2 home: #A4A4A4

--- src/components/SolucoesSection.js
+++ src/components/SolucoesSection.js
@@ -1,53 +1,64 @@
-import React from 'react'
+import React from 'react';
 import AnchorLink from 'react-anchor-link-smooth-scroll';
 
 import Card from './Card';
-import LineButton from './LineButton'
+import LineButton from './LineButton';
 
 import DevIcon from '../svg/DevIcon';
 import BulbIcon from '../svg/BulbIcon';
 import ClassIcon from '../svg/ClassIcon';
 
 const SolucoesSection = () => (
-  <section id="solucoes" className="my-20 py-16 pb-20 lg:mb-40 lg:mt-48" style={{background: "linear-gradient(29deg, #5B87B5, #3B5F9E 70%)"}}>
-  <div className="container mx-auto text-center">
-    <h2 className="text-3xl lg:text-5xl  text-white mx-4 md:mx-0">Veja como podemos ajudar a sua empresa</h2>
-    <p className="mt-4 text-white text-xl font-thin mx-4 md:mx-0">Implemente, treine, desenvolva ou receba suporte nas ferramentas da Microsoft</p>
-    <div className="flex flex-col md:flex-row md:-mx-3 mt-12">
-      <div className="flex-1 px-3 m-6 md:m-2">
-        <Card className="p-12 h-full">
-          <DevIcon />
-          <p className="font-semibold text-xl">Projeto</p>
-          <p className="mt-4 mb-8">
-          Desenvolva projetos personalizados para sua empresa. Planilhas automatizadas com VBA, dashboards, aplicativos empresariais, formulários, processos automatizados e muito mais com as ferramentas mais integradas no mercado.
-          </p>
-        </Card>
+  <section
+    id="solucoes"
+    className="my-20 py-16 pb-20 lg:mb-40 lg:mt-48"
+    style={{ background: 'linear-gradient(29deg, #5B87B5, #3B5F9E 70%)' }}
+  >
+    <div className="container mx-auto text-center">
+      <h2 className="text-3xl lg:text-5xl  text-white mx-4 md:mx-0">
+        Veja como podemos ajudar a sua empresa
+      </h2>
+      <p className="mt-4 text-white text-xl font-thin mx-4 md:mx-0">
+        Implemente, treine, desenvolva ou receba suporte nas ferramentas da Microsoft
+      </p>
+      <div className="flex flex-col md:flex-row md:-mx-3 mt-12">
+        <div className="flex-1 px-3 m-6 md:m-2">
+          <Card className="p-12 h-full">
+            <DevIcon />
+            <p className="font-semibold text-xl">Projeto</p>
+            <p className="mt-4" style={{ color: '#A4A4A4' }}>
+              Desenvolva projetos personalizados para sua empresa. Planilhas automatizadas com VBA,
+              dashboards, aplicativos empresariais, formulários, processos automatizados e muito
+              mais com as ferramentas mais integradas no mercado.
+            </p>
+          </Card>
+        </div>
+        <div className="flex-1 px-3 m-6 md:m-2">
+          <Card className="p-12 h-full">
+            <BulbIcon />
+            <p className="font-semibold text-xl">Consultoria</p>
+            <p className="mt-4" style={{ color: '#A4A4A4' }}>
+              Uma das melhores opções para quem usa diariamente as ferramentas da Microsoft,
+              permitindo contratar suporte e desenvolvimento remoto de um especialista.
+            </p>
+          </Card>
+        </div>
+        <div className="flex-1 px-3 m-6 md:m-2">
+          <Card className="p-12 h-full">
+            <ClassIcon />
+            <p className="font-semibold text-xl">Treinamento</p>
+            <p className="mt-4" style={{ color: '#A4A4A4' }}>
+              Treinamentos in-company para qualificar a sua equipe nas principais ferramentas do
+              mercado, aumentar a produtividade e abrir portas para novas possibilidades.
+            </p>
+          </Card>
+        </div>
       </div>
-      <div className="flex-1 px-3 m-6 md:m-2">
-        <Card className="p-12 h-full">
-          <BulbIcon />
-          <p className="font-semibold text-xl">Consultoria</p>
-          <p className="mt-4">
-          Uma das melhores opções para quem usa diariamente as ferramentas da Microsoft, permitindo contratar suporte e desenvolvimento remoto de um especialista.
-          </p>
-        </Card>
-      </div>
-      <div className="flex-1 px-3 m-6 md:m-2">
-        <Card className="p-12 h-full">
-          <ClassIcon />
-          <p className="font-semibold text-xl">Treinamento</p>
-          <p className="mt-4">
-          Treinamentos in-company para qualificar a sua equipe nas principais ferramentas do mercado, aumentar a produtividade e
-abrir portas para novas possibilidades.
-          </p>
-        </Card>
-      </div>
+      <AnchorLink href="#ferramentas">
+        <LineButton className="mt-12 flex mx-auto">Plataformas</LineButton>
+      </AnchorLink>
     </div>
-    <AnchorLink href="#ferramentas">
-    <LineButton className="mt-12 flex mx-auto">Plataformas</LineButton>
-    </AnchorLink>
-  </div>
-</section>
-)
+  </section>
+);
 
-export default SolucoesSection
+export default SolucoesSection;
