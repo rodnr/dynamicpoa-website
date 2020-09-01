e7a2df30dc8e6ef891487d7f1a31ecf218610566
Rafael Muttoni <hi@rafaelmuttoni.com>
2020-08-31 21:19:55 -0300

    swiper-slide position fix

--- src/components/Caroussel/styled.js
+++ src/components/Caroussel/styled.js
@@ -3,4 +3,10 @@ import styled from 'styled-components';
 export const Wrapper = styled.section`
   margin-top: 2rem;
   margin-bottom: 6rem;
+
+  .swiper-slide {
+    img {
+      margin: 0 auto;
+    }
+  }
 `;
