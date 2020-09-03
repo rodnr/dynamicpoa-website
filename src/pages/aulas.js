3505daf9e2eafd61440982dd05220adf0c61665c
Rafael Muttoni <hi@rafaelmuttoni.com>
2020-09-03 20:19:30 -0300

    aulas same lottie

--- src/pages/aulas.js
+++ src/pages/aulas.js
@@ -4,8 +4,7 @@ import SEO from '../components/seo';
 
 import Layout from '../components/layout/Layout';
 import Button from '../components/Button';
-import DynamicIcon from '../svg/DynamicIcon';
-import LottieProgramming from '../components/LottieProgramming';
+import LottieHero from '../components/LottieHero';
 
 import ClassesPlatforms from '../components/ClassesPlatforms';
 import ClassesBenefits from '../components/ClassesBenefits';
@@ -19,7 +18,7 @@ const AulasPage = () => (
     <section className="pt-20" id="top">
       <div className="container mx-auto px-8 lg:flex items-center">
         <div className="text-left p-4 lg:pr-16 lg:w-1/2">
-          <h1 className="hero-title">Aulas particulares personalizadas para você</h1>
+          <h1 className="hero-title">Aulas particulares personalizadas</h1>
           <p className="hero-description">
             Aprofunde o seu conhecimentos nas ferramentas da Microsoft. Aprofunde o seu
             conhecimentos nas ferramentas da Microsoft. Aprofunde o seu conhecimentos nas
@@ -29,8 +28,8 @@ const AulasPage = () => (
             <Button>Ver preços</Button>
           </AnchorLink>
         </div>
-        <div className="m-0 hidden lg:w-2/5 lg:block" style={{ margin: '0 auto' }}>
-          <LottieProgramming />
+        <div className="m-0 hidden lg:w-3/5 lg:block">
+          <LottieHero />
         </div>
       </div>
     </section>
