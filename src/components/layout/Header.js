3504fd5f0a11a6caf1769b96fd2ce5725e5e0649
rodnr <64420738+rodnr@users.noreply.github.com>
2020-09-04 03:33:53 -0300

    Link para forms

--- src/components/layout/Header.js
+++ src/components/layout/Header.js
@@ -25,7 +25,7 @@ const Header = () => (
       <Link to="/precos" className="px-4 font-bold mt-3 md:mt-0">
         Preços
       </Link>
-      <a href="" target="_blank" className="px-4 font-bold mt-3 md:mt-0 lg:mr-4">
+      <a href="https://forms.office.com/Pages/ResponsePage.aspx?id=rBUgpJB7I06awUoXtAxN1O45KqynsXxChkWOBFtkIIdUNURBTkRHTloyU1ZFTEY3TTJVS1dBT0xZMS4u" target="_blank" className="px-4 font-bold mt-3 md:mt-0 lg:mr-4">
         Vagas
       </a>
       <a
