b8db6619d2aae8d4b60364fb87397771e85e4634
Lucas Klein <lucas.fklein@hotmail.com>
2023-03-21 12:37:06 -0300

    styles

--- src/components/BlogList/styled.js
+++ src/components/BlogList/styled.js
@@ -1,13 +1,13 @@
 import styled from "styled-components";
 
 export const GridContainer = styled.div`
-  width: 90%;
+  width: 80%;
   margin-left: auto;
   margin-right: auto;
   display: grid;
   grid-template-columns: repeat(3, 1fr); // 3 columns for desktop
   grid-auto-rows: minmax(600px, auto);
-  gap: 0 20px; // gap between grid items
+  gap: 40px; // gap between grid items
   @media screen and (max-width: 1024px) {
     grid-template-columns: repeat(2, 1fr); // 2 columns for tablet
   }
