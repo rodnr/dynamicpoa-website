4b876b7d1bfcd1747c924a6303d7d84c6a4ca97a
Klein <116522160+lucasfeklein@users.noreply.github.com>
2023-03-23 15:00:05 -0300

    post media query added

--- src/components/Post/styled.js
+++ src/components/Post/styled.js
@@ -5,6 +5,12 @@ export const PostHeader = styled.header`
   margin: auto;
   max-width: 70rem;
   padding: 5rem 5rem 0;
+
+  @media screen and (max-width: 768px) {
+    font-size: 2rem;
+    padding: 3rem 1rem 0;
+  }
+
 `
 
 export const PostTitle = styled.h1`
@@ -13,24 +19,42 @@ export const PostTitle = styled.h1`
   padding: 0 1.4rem;
   margin: 1rem auto;
   line-height: 1.2;
+
+  @media screen and (max-width: 768px) {
+    font-size: 2.8rem;
+  }
 `
 
 export const PostDescription = styled.h2`
   font-size: 2rem;
   font-weight: 200;
   padding: 0 1.4rem;
+
+  @media screen and (max-width: 768px) {
+    font-size: 1.6rem;
+  }
 `
 
 export const PostDate = styled.p`
   font-size: 1.1rem;
   font-weight: 100;
   padding: 0 1.4rem;
+
+  @media screen and (max-width: 768px) {
+    font-size: 0.9rem;
+  }
 `
 
 export const MainContent = styled.section`
   margin: auto;
   max-width: 70rem;
   padding: 2rem 5rem;
+
+  @media screen and (max-width: 768px) {
+    font-size: 2rem;
+    padding: 3rem 1rem 0;
+  }
+
   p,
   h1,
   h2,
@@ -48,9 +72,11 @@ export const MainContent = styled.section`
     letter-spacing: 0.069rem;
     padding: 0 1.4rem;
   }
+
   p {
     margin: 0 auto 1.6rem;
   }
+
   h1,
   h2,
   h3,
@@ -58,47 +84,56 @@ export const MainContent = styled.section`
   h5 {
     margin: 2.4rem auto 1rem;
   }
+
   ul,
   ol {
     list-style: disc;
     padding-left: 2.5rem;
     margin: 0 auto 1.6rem;
   }
+
   li {
     padding: 0.625rem 0;
     & > ul {
       margin-bottom: 0;
     }
   }
+
   p,
   li {
     code {
       word-wrap: break-word;
     }
   }
+
   img {
     display: inline-block;
     max-width: 100%;
   }
+
   iframe {
     padding: 0 1.6rem 1.6rem;
     width: 100%;
   }
+
   blockquote {
     color: #fff;
     border-left: 0.3rem solid #1fa1f2;
     padding: 0 1.875rem;
     margin: 3.125rem auto;
   }
+
   hr {
     border: 1px solid #38444d;
     margin: 3rem auto;
   }
+
   #twitter-widget-0,
   .instagram-media,
   .twitter-tweet {
     margin: 20px auto !important;
   }
+
   h1,
   h2,
   h3,
@@ -108,28 +143,36 @@ export const MainContent = styled.section`
     letter-spacing: 0.069rem;
     line-height: 1.4;
   }
+
   h1 {
     font-size: 2.8rem;
   }
+
   h2 {
     font-size: 2.1rem;
   }
+
   h3 {
     font-size: 1.6rem;
   }
+
   h4 {
     font-size: 1.4rem;
   }
+
   h5 {
     font-size: 1.2rem;
   }
+
   strong {
     font-weight: 700;
   }
+
   .gatsby-resp-image-background-image {
     z-index: 2;
     opacity: 1 !important;
   }
+
   .gatsby-resp-image-image {
     box-shadow: none !important;
     transition: opacity 0.2s;
@@ -141,12 +184,15 @@ export const MainContent = styled.section`
       z-index: 3;
     }
   }
+
   .gatsby-highlight {
     padding: 0 1.6rem 1.6rem;
   }
+
   .instagram-media {
     margin: 1rem auto !important;
   }
+
   a {
     border-bottom: 1px dashed #1fa1f2;
     color: #1fa1f2;
@@ -155,8 +201,10 @@ export const MainContent = styled.section`
     svg {
       color: #fff;
     }
+
     &:hover {
       opacity: 0.8;
     }
+
   }
 `
