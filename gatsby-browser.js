a32b5f21e879b97faf27de8112e06f91cf0302e0
Lucas Klein <lucas.fklein@hotmail.com>
2023-03-22 23:52:48 -0300

    giigke tag and hubspot added

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,3 +1,26 @@
 import "lazysizes";
 import './src/css/tailwind.css';
 
+
+export const onClientEntry = () => {
+    // Google tag (gtag.js)
+    const script = document.createElement("script")
+    script.src = "https://www.googletagmanager.com/gtag/js?id=AW-11059828868"
+    script.async = true
+    document.body.appendChild(script)
+
+    window.dataLayer = window.dataLayer || []
+    function gtag() { dataLayer.push(arguments); }
+    gtag('js', new Date());
+
+    gtag('config', 'AW-11059828868')
+
+    // HubSpot Embed Code
+    const hsScript = document.createElement("script")
+    hsScript.type = "text/javascript"
+    hsScript.id = "hs-script-loader"
+    hsScript.async = true
+    hsScript.defer = true
+    hsScript.src = "//js.hs-scripts.com/22189317.js"
+    document.body.appendChild(hsScript)
+}
