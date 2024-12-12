90f5ff9cd76e96bd85c2a00af66872821947411a
rodnr <64420738+rodnr@users.noreply.github.com>
2024-12-12 11:40:00 -0300

    Link corrigido

--- src/data/planilhas-data.js
+++ src/data/planilhas-data.js
@@ -3,7 +3,7 @@ export default [
     title: 'Gestão de Leads',
     description:
       'Registre potenciais clientes e faça o acompanhamento das datas de contatos para visualizar através de gráficos o seu funil de vendas.',
-    img: 'https://imgur.com/a/XSTMYBA',
+    img: 'https://i.imgur.com/30mGbcB.jpeg',
     downloadLink: 'https://bit.ly/planilha-gestao-leads-dyn0001',
     id: 1
   },
@@ -11,7 +11,7 @@ export default [
     title: 'Gestão Financeira',
     description:
       'Cadastre metas de economia e faça o controle de todos os gastos e despesas pessoais nessa planilha interativa, com gráficos para acompanhamento do seu orçamento mensal.',
-    img: 'https://imgur.com/a/drFNDA8',
+    img: 'https://i.imgur.com/MoqhWEP.jpeg',
     downloadLink: 'https://bit.ly/planilha-gestao-financeira-dyn0002',
     id: 2
   }
