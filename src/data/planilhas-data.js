29f9ad72db1dcb583898915ef38239bb3451ae8e
Rafael Muttoni <rafael@muttoni.com.br>
2024-12-12 21:05:03 +0700

    fix issue

--- src/data/planilhas-data.js
+++ src/data/planilhas-data.js
@@ -1,16 +1,18 @@
 export default [
   {
-    title:'Gestão de Leads',
-    description:'Registre potenciais clientes e faça o acompanhamento das datas de contatos para visualizar através de gráficos o seu funil de vendas.',
-    img:'https://imgur.com/a/XSTMYBA,
+    title: 'Gestão de Leads',
+    description:
+      'Registre potenciais clientes e faça o acompanhamento das datas de contatos para visualizar através de gráficos o seu funil de vendas.',
+    img: 'https://imgur.com/a/XSTMYBA',
     downloadLink: 'https://bit.ly/planilha-gestao-leads-dyn0001',
     id: 1
   },
   {
-    title:'Gestão Financeira',
-    description:'Cadastre metas de economia e faça o controle de todos os gastos e despesas pessoais nessa planilha interativa, com gráficos para acompanhamento do seu orçamento mensal.',
-    img:'https://imgur.com/a/drFNDA8',
+    title: 'Gestão Financeira',
+    description:
+      'Cadastre metas de economia e faça o controle de todos os gastos e despesas pessoais nessa planilha interativa, com gráficos para acompanhamento do seu orçamento mensal.',
+    img: 'https://imgur.com/a/drFNDA8',
     downloadLink: 'https://bit.ly/planilha-gestao-financeira-dyn0002',
     id: 2
-  },
-]
+  }
+];
