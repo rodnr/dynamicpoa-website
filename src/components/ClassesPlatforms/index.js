c629e60bc21c744076b02a926c59a4b5092bd06f
Rafael Muttoni <rafael@muttoni.com.br>
2024-12-12 21:09:53 +0700

    fix more issues

--- src/components/ClassesPlatforms/index.js
+++ src/components/ClassesPlatforms/index.js
@@ -1,39 +1,31 @@
 import React, { useState } from 'react';
 
-import { FaGraduationCap , FaCalculator, FaCode , FaChartPie , FaChalkboardTeacher, FaFileAlt , FaMobileAlt, FaChartBar , FaPalette , FaDatabase , FaSitemap} from 'react-icons/fa';
+import { FaChartBar } from 'react-icons/fa';
 
-import * as S from './styled';
 import ClassesPlan from '../ClassesPlan';
+import * as S from './styled';
 
-import excelIcon from '../../images/apps/Excel.svg';
-import pptIcon from '../../images/apps/PPT.svg';
-import wordIcon from '../../images/apps/Word.svg';
 import powerBIIcon from '../../images/apps/PowerBI.svg';
-import powerAppsIcon from '../../images/apps/PowerApps.svg';
-import flowIcon from '../../images/apps/Flow.svg';
 
 const ClassesPlatforms = () => {
   const [platform, setPlatform] = useState(null);
 
   return (
     <S.PlatformsSection id="platformsSection">
-      <S.PlatformsTitle>Inscreva-se nos próximos treinamentos online abertos ao público</S.PlatformsTitle>
+      <S.PlatformsTitle>
+        Inscreva-se nos próximos treinamentos online abertos ao público
+      </S.PlatformsTitle>
       <S.PlatformsWrapper>
-           
-       
         <S.PlatformBox
           className={platform === 'Power BI' && 'active'}
           onClick={() => setPlatform('Power BI')}
         >
           <img src={powerBIIcon} alt="Power BI" />
         </S.PlatformBox>
-      
-     
       </S.PlatformsWrapper>
       {platform !== null && <S.PlatformTitle>{platform}</S.PlatformTitle>}
       {platform !== null && (
         <S.PlansWrapper>
-        
           {platform === 'Power BI' && (
             <>
               <ClassesPlan
@@ -44,10 +36,8 @@ const ClassesPlatforms = () => {
                 price="20/01/2025"
                 link="https://buy.stripe.com/5kAbL957R9eJd4Q5kl"
               />
-                            
             </>
-  
-         
+          )}
         </S.PlansWrapper>
       )}
     </S.PlatformsSection>
