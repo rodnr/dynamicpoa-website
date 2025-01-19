179153bde70b2cafcc044b243d0c2e11224c1ba9
rodnr <64420738+rodnr@users.noreply.github.com>
2025-01-19 16:19:16 -0300

    teste

--- src/pages/index.js
+++ src/pages/index.js
@@ -50,7 +50,7 @@ export default () => (
           </p>
 
           <div className="text-center mt-16">
-            <img className="w-1/10 sm:w-1/10 my-0 mx-auto" src="/assets/img/microsoft-partner.png" />
+            <img className="w-1/5 sm:w-2/5 my-0 mx-auto" src="/assets/img/microsoft-partner.png" />
           </div>
           </div>
 
