ec4f934e19a848fed6afa464de777b4a722b02cc
rodnr <64420738+rodnr@users.noreply.github.com>
2025-05-05 15:21:24 -0300

    Alterações nos textos

--- src/components/SolucoesSection.js
+++ src/components/SolucoesSection.js
@@ -16,17 +16,17 @@ const SolucoesSection = () => (
   >
     <div className="container mx-auto text-center">
       <h2 className="text-3xl lg:text-5xl  text-white mx-4 md:mx-0">
-        Veja como podemos ajudar a sua empresa
+      End-to-end services to empower your Microsoft environment
       </h2>
       <p className="mt-4 text-white text-xl font-thin mx-4 md:mx-0">
-        Utilize todos os recursos que as ferramentas do Microsoft 365 e Plataforma Power podem oferecer 
+      Adaptable options for teams of any size and level of maturity.
       </p>
       <div className="flex flex-col md:flex-row md:-mx-3 mt-12">
         <div className="flex-1 px-3 m-6 md:m-2">
           <Card className="p-12 h-full">
             <img src="/assets/img/solutions/Projetos.png" className="w-2/6 my-0 mx-auto"/>
-            <p className="font-semibold text-xl" style={{ color: '#1C2A68' }}>Licenciamento</p>
-            <p className="mt-4" style={{ color: '#909090' }}>
+            <p className="font-semibold text-xl" style={{ color: '#202A45' }}>Licensing</p>
+            <p className="mt-4" style={{ color: '#EBE8E2' }}>
               Adquira licenças da Microsoft com nossos descontos de até 20% diretamente com a nossa empresa, que é revendedora oficial. 
               Faça cotações para serviços como Azure, Dynamics 365, Microsoft 365, Power BI, Exchange, Windows e muito mais. 
             </p>
@@ -35,8 +35,8 @@ const SolucoesSection = () => (
         <div className="flex-1 px-3 m-6 md:m-2">
           <Card className="p-12 h-full">
             <img src="/assets/img/solutions/Consultoria.png" className="w-2/6 my-0 mx-auto"/>
-            <p className="font-semibold text-xl" style={{ color: '#1C2A68' }}>Consultoria</p>
-            <p className="mt-4" style={{ color: '#909090' }}>
+            <p className="font-semibold text-xl" style={{ color: '#202A45' }}>Administration</p>
+            <p className="mt-4" style={{ color: '#EBE8E2' }}>
               Contrate especialistas nas ferramentas do Microsoft 365 e Plataforma Power para ficar à disposição da sua empresa através de pacotes mensais de Suporte, 
               Desenvolvimento ou Gerenciamento.
             </p>
@@ -45,8 +45,8 @@ const SolucoesSection = () => (
         <div className="flex-1 px-3 m-6 md:m-2">
           <Card className="p-12 h-full">
             <img src="/assets/img/solutions/Treinamentos.png" className="w-2/6 my-0 mx-auto"/>
-            <p className="font-semibold text-xl" style={{ color: '#1C2A68' }}>Treinamento</p>
-            <p className="mt-4" style={{ color: '#909090' }}>
+            <p className="font-semibold text-xl" style={{ color: '#202A45' }}>Support</p>
+            <p className="mt-4" style={{ color: '#EBE8E2' }}>
               Treinamentos corporativos in-company ou online elaborados 
               de forma personalizada para a sua empresa, trazendo aplicações práticas para os setores e 
               qualificando os colaboradores nas principais ferramentas do
@@ -55,9 +55,7 @@ const SolucoesSection = () => (
           </Card>
         </div>
       </div>
-      <AnchorLink href="#ferramentas">
-        <LineButton className="mt-12 flex mx-auto">Conheça as plataformas</LineButton>
-      </AnchorLink>
+     
     </div>
   </section>
 );
