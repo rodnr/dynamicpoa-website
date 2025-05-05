29b74e0e43509d249ac67eb265bac019e4a2cb3d
rodnr <64420738+rodnr@users.noreply.github.com>
2025-05-05 16:20:14 -0300

    Alteração no slogan

--- src/pages/index.js
+++ src/pages/index.js
@@ -23,9 +23,9 @@ export default () => (
     <section className="pt-20" id="top">
       <div className="container mx-auto px-8 lg:flex items-center">
         <div className="text-left p-4 lg:pr-16 lg:w-1/2">
-          <h1 className="hero-title">Improve your Power Platform management</h1>
+          <h1 className="hero-title">Trust an official Microsoft Partner for your Power Platform solutions</h1>
           <p className="hero-description">
-            Take your environment management to a new level with our team with global projects experiences
+          Top-level management for your environment, with 24/7 technical support available for users
           </p>
 
           <AnchorLink className="text-white hover:text-white" href="#solucoes">
